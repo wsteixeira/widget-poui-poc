f202b921e2b0721d6ca301c6e997d4bc6294eb88
Wanderley Santos Teixeira <wsteixeira@yahoo.com.br>
2023-08-24 10:23:06 -0300

    Ajuste para o build

--- src/main/po-fluig-poc/src/app/app.component.ts
+++ src/main/po-fluig-poc/src/app/app.component.ts
@@ -5,14 +5,16 @@ import { PoMenuItem } from '@po-ui/ng-components';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   readonly menus: Array<PoMenuItem> = [
-    { label: 'Home', action: this.onClick.bind(this) },
+    { label: 'Home', action: this.onClick.bind(this) }
   ];
 
   private onClick() {
-    alert('Clicked in menu item');
+    alert('Clicked in menu item')
   }
+
 }
